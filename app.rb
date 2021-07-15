14b5ef175bace5390fb4077de1d73a58a31f48f6
Codi <codi.kraus@gmail.com>
2021-07-15 11:35:24 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Codi Kraus."
+    end
+
+    get '/hometown' do
+        "My hometown is Austin, Texas."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Tom Sawyer'"
+    end
 end
